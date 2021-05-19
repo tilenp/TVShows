861b46a589b9b5080b65b7e0495c2ab14bbf50d6
tilenp <tilen.prepeluh@gmail.com>
2021-05-19 17:14:45 +0200

    add toolbar

--- app/src/main/java/com/example/tvshows/ui/MainActivity.kt
+++ app/src/main/java/com/example/tvshows/ui/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.tvshows.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.tvshows.R
+import com.example.tvshows.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpToolbar()
+    }
+
+    private fun setUpToolbar() {
+        val navController = (supportFragmentManager.findFragmentById(R.id.main_host_fragment) as NavHostFragment).navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.myToolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
